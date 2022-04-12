9bc993c8e0769a683f1bfaff300bcd128e4e80de
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2022-04-12 13:19:26 +0300

    fixid reducer

--- src/Redux/phonebook/phonebook-reducer.js
+++ src/Redux/phonebook/phonebook-reducer.js
@@ -16,13 +16,13 @@ import {
 
 const contacts = createReducer([],
   {
-    [fetchContactSuccess]: (state, { payload }) => payload,
+    [fetchContactSuccess]: (_, { payload }) => payload,
     [addContactSuccess]: (state, { payload }) => { 
-         if (state.find(contact => contact.nick === payload.nick)) {
-        const notify = `${payload.nick} is already on list`;
-        toast.error(notify);
-        return state;
-      }
+      //    if (state.find(contact => contact.nick === payload.nick)) {
+      //   const notify = `${payload.nick} is already on list`;
+      //   toast.error(notify);
+      //   return state;
+      // }
       state = [...state, payload];
       toast.success(`Contact ${payload.nick} added!`);
       return state;
